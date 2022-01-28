ae3a7ede2eeb46fdb6b99fdb1788e2b028cacd9d
HwijaeCho <p0l1mu98@gmail.com>
2022-01-28 15:57:53 +0900

    220128

--- 11.py
+++ 11.py
@@ -1,26 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# csv 파일 읽기
 filename = './single_crystal_test_out.csv'
 data = np.genfromtxt(filename, dtype=float, delimiter=',', names=True)
 
+# 원하는 값을 열의 제목으로 추출
 x = data['e_zz']
 y = data['stress_zz']
 y2 = data['pk2']
 
-#fontname
+# fontname 또한 설정 가능(검색 ㄱㄱ)
 
 plt.plot(x,y, marker='o', linestyle='-', color='g')
 plt.plot(x,y2, marker='x', color='r')
 
 plt.title('Stress-Strain', fontsize=20)
 
+# xlabel : x축 제목
+# xticks : 최소,최대,주기
+# xlim : 축에 나타내는 값의 범위
 plt.xlabel('e_zz', fontsize=15)
 plt.xticks(np.arange(min(x),max(x),0.0005), fontsize=9)
 plt.xlim([0,0.005])
 
 plt.ylabel('stress_zz', fontsize=15)
-plt.yticks(np.arange(min(y),170,40), fontsize=11)
+plt.yticks(np.arange(min(y),161,40), fontsize=11)
 plt.ylim([0,170])
 
 plt.legend(["stress_zz","pk2"])
--- function1.py
+++ function1.py
@@ -2,10 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as sci
 
+# 1 inch = 2.54 cm
+cm = 1/2.54
+
 xmin = 0
 xmax = 1
 resolution = 100
 
+# 좌표 생성
 x = np.linspace(xmin,xmax,resolution)
 
 y = x**2
@@ -17,17 +21,36 @@ y6 = 1/2 + np.tanh((x-0.5)/0.01)/2
 
 y7 = sci.erf(x)
 
-plt.plot(x,y)
-plt.plot(x,y2)
-plt.plot(x,y3)
-plt.plot(x,y4)
-plt.plot(x,y5)
-plt.plot(x,y6)
-plt.plot(x,y7)
+# dpi 설정, 그래프크기(figsize) 결정
+# https://www.elsevier.com/authors/policies-and-guidelines/artwork-and-media-instructions/artwork-sizing
+fig = plt.figure(dpi=1000)
+fig,ax=plt.subplots(figsize=(19*cm,19*cm))
+
+# 각 그래프 곡선 별 설정(색상, 마커, 선 종류, 선 굵기, 마커 크기, 마커 주기)
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.plot.html#matplotlib.axes.Axes.plot
+ax.plot(x,y, color='#FF3333', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y2, color='#FF9900', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y3, color='#81c147', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y4, color='#008d62', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y5, color='#4aa8d8', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y6, color='#00498c', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
+ax.plot(x,y7, color='#8b00FF', marker='.', linestyle='solid', linewidth=2, markersize=9, markevery=2)
 
 plt.xlabel("x")
 plt.ylabel("y")
 plt.yticks(np.arange(-2,1.1,0.25))
 plt.ylim([-2,1.1])
 
-plt.legend(["y","y2","y3","y4","y5","y6","y7"], loc='lower left')
+# 범례에 수식 넣기
+# https://matplotlib.org/stable/tutorials/text/mathtext.html
+plt.legend([r'$y=x^2$',
+            r'$y_2=\mathrm{sin}(2 \pi x)$',
+            r'$y_3=\mathrm{cos}(2 \pi x)$',
+            r'$y_4=x \mathrm{log}x+(1-x)\mathrm{log}(1-x)$',
+            r'$y_5=e^{-x^2}$',
+            r'$y_6=\frac{1}{2} + \frac{1}{2} \mathrm{tanh}(\frac{x-0.5}{0.01})$',
+            r'$y_7=erf(x)$'], loc='lower left', ncol=2)
+
+# 그래프를 이미지 파일로 저장(위의 dpi와 맞춰주자)
+# eps 또는 tiff 사용 권장
+plt.savefig('function.png', format='png', bbox_inches='tight', dpi=1000)
